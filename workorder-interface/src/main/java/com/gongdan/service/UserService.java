bd5bfc941df33ea9ce50fffd38f2d83f769cab70
yanyiqi1264 <yanyiqi1264@126.com>
2016-05-30 22:41:39 +0800

    edit

--- workorder-interface/src/main/java/com/gongdan/service/UserService.java
+++ workorder-interface/src/main/java/com/gongdan/service/UserService.java
@@ -1,10 +1,14 @@
 package com.gongdan.service;
 
-import com.gongdan.entity.User;
+import com.gongdan.common.entity.User;
 
 public interface UserService {
 	
 	
 	public User getUserInfo(String userNum);
+	
+	public User doUserLogin(String userNum,String password);
 
+	
+	public void changePwd(String userNum,String oldPwd,String newPwd);
 }
